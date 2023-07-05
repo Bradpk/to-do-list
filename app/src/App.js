4830a3cd98abca4290a59314c5819574907f060c
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-07-05 18:07:32 -0400

    g

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import InputComponent from './Components/Input';
 import HeaderComponent from './Components/Header';
+import ListComponent from './Components/List';
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent />
-    <InputComponent />
+    <ListComponent />
     </div>
   );
 }
--- app/src/Components/List.js
+++ app/src/Components/List.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import InputComponent from './Input'
 
 function ListComponent() {
+    const [toDoList, setToDoList] = useState([])
+
+    const addItem = () => {
+        
+    }
+
     return (
         <div>
-
+            <InputComponent />
         </div>
     )
 }
