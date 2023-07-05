82520c4225429f17faa192dbcaacdbcd6751a64c
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-07-05 17:57:52 -0400

    g

--- app/src/Components/Input.js
+++ app/src/Components/Input.js
@@ -1,19 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-function InputComponent() {
+function InputComponent(props) {
     const [userInput, setUserInput] = useState('')
+
 
 
     const handleSubmit = (e) => {
         e.preventDefault()
+
+        // props.onSubmit({
+        //     id: Math.floor(Math.random() * 100000),
+        //     text: userInput
+        // })
+setUserInput('Cat')
+    }
+
+    const handleChange = (e) => {
+setUserInput(e.target.value)
     }
 
     return (
         <div>
-<form className='toDoForm' onSubmit={handleSubmit}>
-    <input type='text'></input>
-    <button>Add Item</button>
-</form>
+            <form className='toDoForm' onSubmit={handleSubmit}>
+                <input type='text' value={userInput} onChange={handleChange}></input>
+                <button>Add Item</button>
+            </form>
         </div>
     )
 }
