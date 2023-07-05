408d93293fa2b3d6b05ce60e33fdfda1d09f9712
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-07-05 17:26:17 -0400

    g

--- app/src/App.js
+++ app/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import InputComponent from './Components/Input';
+import HeaderComponent from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+      <HeaderComponent />
     <InputComponent />
     </div>
   );
--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 function HeaderComponent() {
     return (
         <div>
-
+<h1>To Do List</h1>
         </div>
     )
 }
