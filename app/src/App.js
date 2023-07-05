7fc9db3eb3368a2e8f1875a31b77648f4b4c276c
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-07-05 16:20:41 -0400

    g

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+function App() {
+  return (
+    <div className="App">
+     Hi
+    </div>
+  );
+}
+
+export default App;
