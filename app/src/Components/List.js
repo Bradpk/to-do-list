5473c398678b5e49e8d33b1717900805b8206ca6
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-07-07 09:53:34 -0400

    g

--- app/src/Components/List.js
+++ app/src/Components/List.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import InputComponent from './Input';
-//---------------------------------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------------------------------
 function ListComponent() {
     const [toDoList, setToDoList] = useState([]);
     const [currentView, setCurrentView] = useState('All');
