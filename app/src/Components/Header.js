ab5bae27f50aca2102afdd77015e28a92ad92d07
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-19 14:29:17 -0500

    g

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -4,6 +4,7 @@ function HeaderComponent() {
     return (
         <div>
             <h1>To Do List</h1>
+            <p>(Once items are added, click them to mark as complete)</p>
         </div>
     )
 }
